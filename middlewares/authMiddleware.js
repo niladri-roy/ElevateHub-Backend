b5f7d99e34ea84320ce1e205d017f1afdc47c1e7
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-17 22:23:37 +0530

    Code for 1. Require SignIn 2. isAdmin Middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,43 @@
+const JWT = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+//Protected Routes token base
+const requireSignIn = (req, res, next) => {
+  try{
+    const decode = JWT.verify(
+      req.headers.authorization, 
+      process.env.JWT_SECRET
+    );
+
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send({
+        success: false,
+        message: "UnAuthorized Access",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      success: false,
+      error,
+      message: "Error in admin Middelwares",
+    });
+  }
+};
+
+module.exports = {
+  requireSignIn,
+  isAdmin
+};
