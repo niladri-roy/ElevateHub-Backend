01a543ac94376161fe0851569955c26d7ad0bc18
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-17 14:40:29 +0530

    updating the server

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const app = require('./app');
-console.log("Server Import Line -> Working Good")
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/database.js");
+// const authRoutes = require("./routes/authRoute.js");
+// const categoryRoutes = require("./routes/categoryRoutes.js");
+// const productRoutes = require("./routes/productRoutes.js");
+const cors = require("cors");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+app.get("/", (req, res) => {
+    res.send("Welcome to The E-commerce ElevateHub Backend");
+})
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.bgCyan.white);
 })
