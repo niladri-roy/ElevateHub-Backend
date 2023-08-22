fb520cf9b234f5927844654e87ab368dd0e73b59
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-22 23:58:56 +0530

    Integrating Order Model

---
+++ models/orderModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "Delivered", "Cancel"],
+    },
+  },
+  { timestamps: true }
+);
+
+console.log("Order Model -> Working Successfully")
+module.exports = mongoose.model("Order", orderSchema);
