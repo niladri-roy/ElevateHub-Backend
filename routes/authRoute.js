e98ebc67929b862efab87449a33e5c0fa5dd6b83
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-23 00:00:52 +0530

    Integrating the 1. Test Controller 2. Forgot Password Controller 3. Update Profile Controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,9 @@ const express = require("express");
 const {
   registerController,
   loginController,
-  testController
+  testController,
+  forgotPasswordController,
+  updateProfileController,
 } = require("../controllers/authController.js");
 
 const {
@@ -16,6 +18,24 @@ const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-router.get("/test", requireSignIn, isAdmin, testController)
+router.get("/test", requireSignIn, isAdmin, testController);
+router.post("/forgot-password", forgotPasswordController)
+
+//protected route user-auth 
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({
+    ok : true
+  })
+});
+
+//protected route admin-auth 
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({
+    ok : true
+  })
+});
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
 
 module.exports = router;
