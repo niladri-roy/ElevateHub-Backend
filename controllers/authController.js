a9622589d3aba95d247697e33b16c489ea8079d4
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-22 23:57:00 +0530

    Import for 1. Forgot Password Controller and 2. Update Profile Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const JWT = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address } = req.body;
+    const { name, email, password, phone, address, answer } = req.body;
     //validations
     if (!name) {
       return res.send({ error: "Name is Required" });
@@ -24,9 +24,9 @@ const registerController = async (req, res) => {
     if (!address) {
       return res.send({ message: "Address is Required" });
     }
-    // if (!answer) {
-    //   return res.send({ message: "Answer is Required" });
-    // }
+    if (!answer) {
+      return res.send({ message: "Answer is Required" });
+    }
     //check user
     const existingUser = await userModel.findOne({ email });
     //existing user
@@ -45,7 +45,7 @@ const registerController = async (req, res) => {
       phone,
       address,
       password: hashedPassword,
-      // answer,
+      answer,
     }).save();
 
     res.status(201).send({
@@ -116,6 +116,43 @@ const loginController = async (req, res) => {
   }
 };
 
+const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({ message: "Email is required" });
+    }
+    if (!answer) {
+      res.status(400).send({ message: "answer is required" });
+    }
+    if (!newPassword) {
+      res.status(400).send({ message: "New Password is required" });
+    }
+    //check
+    const user = await userModel.findOne({ email, answer });
+    //validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Wrong Email Or Answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await userModel.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
+
 const testController = (req, res) => {
   try {
     res.send("Protected Routes");
@@ -125,10 +162,45 @@ const testController = (req, res) => {
   }
 };
 
+const updateProfileController = async (req, res) => {
+  try {
+    const { name, /*email,*/ password, address, phone } = req.body;
+    const user = await userModel.findById(req.user._id);
+    //password
+    if (password && password.length < 6) {
+      return res.json({ error: "Password is required and 6 character long" });
+    }
+    const hashedPassword = password ? await hashPassword(password) : undefined;
+    const updatedUser = await userModel.findByIdAndUpdate(
+      req.user._id,
+      {
+        name: name || user.name,
+        password: hashedPassword || user.password,
+        phone: phone || user.phone,
+        address: address || user.address,
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated SUccessfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error WHile Update profile",
+      error,
+    });
+  }
+};
 
 
 module.exports = {
   registerController,
   loginController,
   testController,
+  forgotPasswordController,
+  updateProfileController,
 }
