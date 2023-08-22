15c69946218fb45c0160e8c85feb4cd56a2c6e1d
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-22 23:55:08 +0530

    Import of 1. Product Route 2. Category Route

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/database.js");
 const authRoutes = require("./routes/authRoute.js");
-// const categoryRoutes = require("./routes/categoryRoutes.js");
-// const productRoutes = require("./routes/productRoutes.js");
+const categoryRoutes = require("./routes/categoryRoutes.js");
+const productRoutes = require("./routes/productRoute.js");
 const cors = require("cors");
+
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 
 app.get("/", (req, res) => {
