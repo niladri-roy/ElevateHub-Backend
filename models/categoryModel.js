142234875941e615e96710be721ab28caa1546c7
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-22 23:58:39 +0530

    Integrating Category Model

---
+++ models/categoryModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+  },
+});
+
+console.log("Category Model -> Working Successfully");
+module.exports = mongoose.model("Category", categorySchema);
