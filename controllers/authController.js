c60ebe593af31ac03e666fec6a37f20a00138a89
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-10-19 00:21:32 +0530

    backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -228,7 +228,7 @@ const getAllOrdersController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error WHile Getting Orders",
+      message: "Error While Getting Orders",
       error,
     });
   }
