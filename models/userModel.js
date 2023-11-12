67c14d5f3b768655d5345c71443df31fc52a1987
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-11-12 19:20:33 +0530

    Update model export to use plural collection name

--- models/userModel.js
+++ models/userModel.js
@@ -37,4 +37,4 @@ const userSchema = new mongoose.Schema(
 );
 
 console.log("User Schema Model -> Working Successfully");
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("users", userSchema);
